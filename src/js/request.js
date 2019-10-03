82f1b0eeb21171f41e3ae45511f8eafa02a3ae5b
Jack Vawdrey <jackvawdrey@Jacks-MacBook-Pro.local>
2019-10-03 11:33:51 -0600

    incomplete: need to add the Folder Id

--- src/js/request.js
+++ src/js/request.js
@@ -14,52 +14,66 @@ export default async () => {
   const workflow = await axios.get(`https://www.wrike.com/api/v4/folders/${keys.WORKFLOW_ID}` ,requestOptions);
   const subs = data[0].childIds;
 
-  const output = {Planned: [], Writing: [], Editing: [], Design: [], Approval: [], Publication: [], Completed: [], Review: []};
+  //const output = {Planned: [], Writing: [], Editing: [], Design: [], Approval: [], Publication: [], Completed: [], Review: []};
+  const output = [];
+  const projects = [];
 
   subs.forEach( async (sub) => {
     const { data: { data: details }  } = await axios.get(`https://www.wrike.com/api/v4/folders/${sub}`, requestOptions);
     //console.log('Sub folder details', details);
-    const projects = details[0].childIds;
+    const phaseObject = {
+      id: sub,
+      title: details.title,
+      projects: []
+    }
+    output.push(phaseObject);
+    projects.concat(details[0].childIds);
+  });
 
-    projects.forEach( async (projectId) => {
-      const { data: { data: details }  } = await axios.get(`https://www.wrike.com/api/v4/folders/${projectId}`, requestOptions);
-      //console.log('Project details', details);
-      const data = details[0];
-      const { project : { customStatusId : status }  } = details[0];
-      data.type = data.title.split(' -')[0];
+  const { data: { data: projectDetails }} = await axios.get(`https://www.wrike.com/api/v4/folders/${projects.join(',')}`);
 
-      switch (status) {
-        case "IEABFWPQJMA53SF4": //Planned
-          output.Planned.push(data);
-          break;
-        case "IEABFWPQJMA53SGG": //Writing
-          output.Writing.push(data);
-          break;
-        case "IEABFWPQJMA53SGQ": //Editing
-          output.Editing.push(data);
-          break;
-        case "IEABFWPQJMA53SHE": //Design
-          output.Design.push(data);
-          break;
-        case "IEABFWPQJMA53SG2": //Approval
-          output.Approval.push(data);
-          break;
-        case "IEABFWPQJMA53SHO": //Publication
-          output.Publication.push(data);
-          break;
-        case "IEABFWPQJMA53SF5": //Publication
-          output.Completed.push(data);
-          break;    
-        case "IEABFWPQJMA53SHZ": //Review
-          output.Review.push(data);
-          break;
-      }
+  projectDetails.forEach(p => {
+    console.log(p);
+  });
 
+  /*projects.forEach( async (projectId) => {
+    const { data: { data: details }  } = await axios.get(`https://www.wrike.com/api/v4/folders/${projectId}`, requestOptions);
+    //console.log('Project details', details);
+    const data = details[0];
+    const { project : { customStatusId : status }  } = details[0];
+    data.type = data.title.split(' -')[0];
 
-    });
+    switch (status) {
+      case "IEABFWPQJMA53SF4": //Planned
+        output.Planned.push(data);
+        break;
+      case "IEABFWPQJMA53SGG": //Writing
+        output.Writing.push(data);
+        break;
+      case "IEABFWPQJMA53SGQ": //Editing
+        output.Editing.push(data);
+        break;
+      case "IEABFWPQJMA53SHE": //Design
+        output.Design.push(data);
+        break;
+      case "IEABFWPQJMA53SG2": //Approval
+        output.Approval.push(data);
+        break;
+      case "IEABFWPQJMA53SHO": //Publication
+        output.Publication.push(data);
+        break;
+      case "IEABFWPQJMA53SF5": //Publication
+        output.Completed.push(data);
+        break;    
+      case "IEABFWPQJMA53SHZ": //Review
+        output.Review.push(data);
+        break;
+    }
+
 
   });
 
-  return output;
 
+  return output;
+  */
 }
